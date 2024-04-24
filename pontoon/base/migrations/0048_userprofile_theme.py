28d0f27e6b826a55739f9a705e4204b8f6d31cb3
Børre Gaup <albbas@gmail.com>
2024-04-24 15:16:15 +0200

    Dependency should not include suffix

--- pontoon/base/migrations/0048_userprofile_theme.py
+++ pontoon/base/migrations/0048_userprofile_theme.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("base", "0046_projectslughistory.py"),
+        ("base", "0046_projectslughistory"),
     ]
 
     operations = [
